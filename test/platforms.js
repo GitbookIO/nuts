9ad816fd519bd33fe51f7d012c72587626da343c
Samy Pessé <samypesse@gmail.com>
2015-08-30 15:37:13 +0200

    Set arch as 32bits by default (except for osx)

--- test/platforms.js
+++ test/platforms.js
@@ -15,21 +15,19 @@ describe('Platforms', function() {
             platforms.detect('RELEASES').should.be.exactly(platforms.WINDOWS_32);
         });
 
+        it('should detect linux', function() {
+            platforms.detect('atom-amd64.deb').should.be.exactly(platforms.LINUX_64);
+        });
+
     });
 
     describe('Resolve', function() {
         var version = {
             platforms: [
                 {
-                    "type": "osx",
+                    "type": "osx_64",
                     "filename": "test-3.3.1-darwin.dmg",
                     "download_url": "https://api.github.com/repos/test/test2/releases/assets/793838",
-                    "download_count": 2
-                },
-                {
-                    "type": "osx_64",
-                    "filename": "test-3.3.1-darwin-x64.dmg",
-                    "download_url": "https://api.github.com/repos/test/test2/releases/assets/793868",
                     "download_count": 2
                 },
                 {
@@ -37,13 +35,54 @@ describe('Platforms', function() {
                     "filename": "test-3.3.1-darwin-x64.zip",
                     "download_url": "https://api.github.com/repos/test/test2/releases/assets/793869",
                     "download_count": 0
+                },
+                {
+                    "type": "windows_32",
+                    "filename": "atom-1.0.9-delta.nupkg",
+                    "size": 1457531,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825732",
+                    "download_count": 55844
+                },
+                {
+                    "type": "windows_32",
+                    "filename": "atom-1.0.9-full.nupkg",
+                    "size": 78181725,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825730",
+                    "download_count": 26987
+                },
+                {
+                    "type": "linux_64",
+                    "filename": "atom-amd64.deb",
+                    "size": 71292506,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825658",
+                    "download_count": 2494
+                },
+                {
+                    "type": "windows_32",
+                    "filename": "atom-windows.zip",
+                    "size": 79815714,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825729",
+                    "download_count": 463
+                },
+                {
+                    "type": "windows_32",
+                    "filename": "AtomSetup.exe",
+                    "size": 78675720,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825728",
+                    "download_count": 5612
                 }
             ]
         };
 
 
         it('should resolve to best platform', function() {
-            platforms.resolve(version, 'osx').filename.should.be.exactly("test-3.3.1-darwin-x64.dmg")
+            platforms.resolve(version, 'osx').filename.should.be.exactly("test-3.3.1-darwin.dmg"),
+            platforms.resolve(version, 'win32').filename.should.be.exactly("AtomSetup.exe")
         });
 
         it('should resolve to best platform with a preferred filetype', function() {
