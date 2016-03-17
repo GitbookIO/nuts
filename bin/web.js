29b85f229cafda63b7b5efed2054a1621a30bef0
Samy Pessé <samypesse@gmail.com>
2016-03-17 13:22:05 +0100

    Proxy assets by default

--- bin/web.js
+++ bin/web.js
@@ -25,7 +25,7 @@ var myNuts = nuts.Nuts({
     timeout: process.env.VERSIONS_TIMEOUT,
     cache: process.env.VERSIONS_CACHE,
     refreshSecret: process.env.GITHUB_SECRET,
-    proxyAssets: Boolean(process.env.DONT_PROXY_ASSETS )
+    proxyAssets: !Boolean(process.env.DONT_PROXY_ASSETS)
 });
 
 // Control access to API
