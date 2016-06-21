a2a08a2b3f7fee18e89cbee1f351a3a2108bee50
Jeff Sloyer <jsloyer@gmail.com>
2016-06-21 12:33:22 -0400

    updated linux support

--- test/platforms.js
+++ test/platforms.js
@@ -17,9 +17,26 @@ describe('Platforms', function() {
         });
 
         it('should detect linux', function() {
-            platforms.detect('atom-amd64.deb').should.be.exactly(platforms.LINUX_64);
             platforms.detect('enterprise-amd64.tar.gz').should.be.exactly(platforms.LINUX_64);
             platforms.detect('enterprise-amd64.tgz').should.be.exactly(platforms.LINUX_64);
+            platforms.detect('enterprise-ia32.tar.gz').should.be.exactly(platforms.LINUX_32);
+            platforms.detect('enterprise-ia32.tgz').should.be.exactly(platforms.LINUX_32);
+        });
+
+        it('should detect debian_32', function() {
+            platforms.detect('atom-ia32.deb').should.be.exactly(platforms.LINUX_DEB_32);
+        });
+
+        it('should detect debian_64', function() {
+            platforms.detect('atom-amd64.deb').should.be.exactly(platforms.LINUX_DEB_64);
+        });
+
+        it('should detect rpm_32', function() {
+            platforms.detect('atom-ia32.rpm').should.be.exactly(platforms.LINUX_RPM_32);
+        });
+
+        it('should detect rpm_64', function() {
+            platforms.detect('atom-amd64.rpm').should.be.exactly(platforms.LINUX_RPM_64);
         });
 
     });
@@ -56,7 +73,47 @@ describe('Platforms', function() {
                     "download_count": 26987
                 },
                 {
+                    "type": "linux_32",
+                    "filename": "atom-ia32.tar.gz",
+                    "size": 71292506,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825658",
+                    "download_count": 2494
+                },
+                {
                     "type": "linux_64",
+                    "filename": "atom-amd64.tar.gz",
+                    "size": 71292506,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825658",
+                    "download_count": 2494
+                },
+                {
+                    "type": "linux_rpm_32",
+                    "filename": "atom-ia32.rpm",
+                    "size": 71292506,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825658",
+                    "download_count": 2494
+                },
+                {
+                    "type": "linux_rpm_64",
+                    "filename": "atom-amd64.rpm",
+                    "size": 71292506,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825658",
+                    "download_count": 2494
+                },
+                {
+                    "type": "linux_deb_32",
+                    "filename": "atom-ia32.deb",
+                    "size": 71292506,
+                    "content_type": "application/zip",
+                    "download_url": "https://api.github.com/repos/atom/atom/releases/assets/825658",
+                    "download_count": 2494
+                },
+                {
+                    "type": "linux_deb_64",
                     "filename": "atom-amd64.deb",
                     "size": 71292506,
                     "content_type": "application/zip",
@@ -84,8 +141,14 @@ describe('Platforms', function() {
 
 
         it('should resolve to best platform', function() {
-            platforms.resolve(version, 'osx').filename.should.be.exactly("test-3.3.1-darwin.dmg"),
-            platforms.resolve(version, 'win32').filename.should.be.exactly("AtomSetup.exe")
+            platforms.resolve(version, 'osx').filename.should.be.exactly("test-3.3.1-darwin.dmg");
+            platforms.resolve(version, 'win32').filename.should.be.exactly("AtomSetup.exe");
+            platforms.resolve(version, 'linux_64').filename.should.be.exactly("atom-amd64.tar.gz");
+            platforms.resolve(version, 'linux_32').filename.should.be.exactly("atom-ia32.tar.gz");
+            platforms.resolve(version, 'linux_rpm_32').filename.should.be.exactly("atom-ia32.rpm");
+            platforms.resolve(version, 'linux_rpm_64').filename.should.be.exactly("atom-amd64.rpm");
+            platforms.resolve(version, 'linux_deb_32').filename.should.be.exactly("atom-ia32.deb");
+            platforms.resolve(version, 'linux_deb_64').filename.should.be.exactly("atom-amd64.deb");
         });
 
         it('should resolve to best platform with a preferred filetype', function() {
