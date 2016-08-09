a1c05a2e62619643ce6ae2ab20b310606ec9ed5a
Muhammad Z. Mousa <mzmousa@users.noreply.github.com>
2016-08-09 15:31:43 -0400

    remove process.env.VCAP_APP_PORT
    Deprecated in CloudFoundry

--- bin/web.js
+++ bin/web.js
@@ -121,7 +121,7 @@ myNuts.init()
 
 // Start the HTTP server
 .then(function() {
-    var server = app.listen(process.env.VCAP_APP_PORT || process.env.PORT || 5000, function () {
+    var server = app.listen(process.env.PORT || 5000, function () {
         var host = server.address().address;
         var port = server.address().port;
 
